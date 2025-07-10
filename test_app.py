610f5d541861a3dd871ae298f0db9e6f195e2b17
saint Claire <saint mbakastclaire@gmail.com>
2025-07-10 12:29:33 +0200

    ++

---
+++ test_app.py
@@ -0,0 +1,12 @@
+from app import app
+
+def test_hello_route():
+    client = app.test_client()
+    response = client.get('/')
+    assert response.status_code == 200
+
+def test_status_route():
+    client = app.test_client()
+    response = client.get('/status')
+    assert response.status_code == 200
+    assert response.get_json()['status'] == 'ok'
